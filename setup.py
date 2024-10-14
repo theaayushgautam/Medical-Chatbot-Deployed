ad12ceb92aeb88705b6e8eddcf0df729d026384a
Aayush Gautam <dipesh.gautam1998@gmail.com>
2024-10-14 19:57:19 -0400

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Aayush Gautam',
+    author_email= 'theaayushgautam@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
